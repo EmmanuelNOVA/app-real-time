402e3bdbaa0ba1aafc9dcfdce09ea5aa400ef507
EmmanuelNOVA <emmanuel.noriegav@gmail.com>
2020-10-02 20:29:57 -0500

    New Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/navigation/sidebar/sidebar.component';
+import { TopbarComponent } from './components/navigation/topbar/topbar.component';
+import { FooterComponent } from './components/navigation/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
