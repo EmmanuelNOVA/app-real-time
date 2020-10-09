7d6fe60f3a20811e2b789701c21376ec35ba6917
EmmanuelNOVA <emmanuel.noriegav@gmail.com>
2020-10-09 18:45:54 -0500

    Incorporar Javascript para funcionamiento sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var App:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-real-time';
+
+  ngOnInit() {
+    //Load Sidebar
+    App.init();
+  }
 }
