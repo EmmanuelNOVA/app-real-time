5477249ea1e07f361929407623e471a051ed3daa
EmmanuelNOVA <emmanuel.noriegav@gmail.com>
2020-10-09 21:56:05 -0500

    Se agrego nFor para toolbar

--- src/app/components/navigation/topbar/topbar.component.ts
+++ src/app/components/navigation/topbar/topbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'navigation-topbar',
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class TopbarComponent implements OnInit {
 
   constructor() { }
-
+  //Declaration Variables
+  @Input() menuItems:any[] = [];
   ngOnInit(): void {
+    
   }
 
 }
